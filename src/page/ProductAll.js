dc0d19fd09d5cc6dd55491e50ead08055776e323
munjehun <miso1489@gmail.com>
2023-01-08 23:59:34 +0900

    API 부르기

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ProductAll = () => {
-  return <div>전체 상품 페이지</div>;
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    let response = await fetch("http://localhost:5000/products");
+    let data = await response.json();
+    setProducts(data);
+  };
+  console.log(products);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      {products.map((product, index) => (
+        <div key={index}>{product}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductAll;
