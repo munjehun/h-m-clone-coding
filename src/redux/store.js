0361ad15f990a1ff7d6f7112df344adfa3aee89e
munjehun <miso1489@gmail.com>
2023-01-18 21:29:26 +0900

    Devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 // rootReducer는 임의로 정한 이름!
 // /index.js는 기본파일이므로 굳이 index.js는 안해줘도 된다.
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+let store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
